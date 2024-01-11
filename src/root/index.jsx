52dae7938735f57429162ac197cd960231df2586
Mirabbosbek9778 <jwicj969@gmail.com>
2024-01-11 22:17:01 +0500

    full proejct open private repository

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from '../components/Dashboard';
-import Login from '../components/Login';
-import Navbar from '../components/Navbar';
-import Register from '../components/Register';
-import { data } from '../utilits/navbar';
-import { sidebar } from '../utilits/sidebar';
-import PrivateRoute from './PrivateRoutes';
-import Course from '../components/Dashboard/Courses/Course';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../components/Dashboard";
+import Login from "../components/Login";
+import Navbar from "../components/Navbar";
+import Register from "../components/Register";
+import { data } from "../utilits/navbar";
+import { sidebar } from "../utilits/sidebar";
+import PrivateRoute from "./PrivateRoutes";
+import Course from "../components/Dashboard/Courses/Course";
 const Root = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navbar />}>
+      <Route path="/" element={<Navbar />}>
         {data?.map(({ id, path, Component }) => (
           <Route key={id} path={path} element={<Component />} />
         ))}
       </Route>
-      <Route path='login' element={<Login />} />
-      <Route path='register' element={<Register />} />
-      <Route path={'*'} element={<div>you are lost</div>} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path={"*"} element={<div>you are lost</div>} />
 
-      <Route path='user' element={<Dashboard />}>
+      <Route path="user" element={<Dashboard />}>
         {sidebar?.map(({ id, path, Component }) => (
           <Route
             key={id}
@@ -32,7 +32,7 @@ const Root = () => {
             }
           />
         ))}
-        <Route path='courses/:courseId' element={<Course />} />
+        <Route path="courses/:courseId" element={<Course />} />
       </Route>
     </Routes>
   );
--- src/utilits/navbar.js
+++ src/utilits/navbar.js
@@ -1,37 +1,37 @@
-import Generic from '../components/Generic';
-import Home from '../components/Home';
-import CourseItem from '../components/Home/Courses/CourseItem';
+import Generic from "../components/Generic";
+import Home from "../components/Home";
+import CourseItem from "../components/Home/Courses/CourseItem";
 
 export const data = [
   {
     id: 1,
-    title: 'Home',
-    path: '/',
+    title: "Home",
+    path: "/",
     Component: Home,
     private: true,
   },
   {
     id: 2,
-    title: 'Courses',
-    path: 'courses',
+    title: "Courses",
+    path: "courses",
     Component: Generic,
   },
   {
     id: 3,
-    title: 'Why Us',
-    path: 'why-us',
+    title: "Why Us",
+    path: "why-us",
     Component: Generic,
   },
   {
     id: 4,
-    title: 'Experts',
-    path: 'experts',
+    title: "Experts",
+    path: "experts",
     Component: Generic,
   },
   {
-    id: '1-1',
-    title: 'Home',
-    path: '/:id',
+    id: "1-1",
+    title: "Home",
+    path: "/:id",
     Component: CourseItem,
     private: true,
   },
